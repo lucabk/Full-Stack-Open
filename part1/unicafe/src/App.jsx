0e3fbe8bf2ff5c819155e17f3ad1a619b797b18a
lucabk <lucabk96l@gmail.com>
2024-09-09 18:06:40 +0200

    ex1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,69 +1,34 @@
-// the file imports the useState function
 import { useState } from 'react'
 
-//Component
-const App = () => {
+const Button = () =>{
 
-  /*The function returns an array that contains two items:
-  The counter variable is assigned the initial value of state, which is zero. 
-  The variable setCounter is assigned a function that will be used to modify the state.
-  When the state modifying function setCounter is called, React re-renders the 
-  component which means that the function body of the component function gets re-executed*/
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  //Event Handlers
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-
-  /*The event handler is passed to the Button component through the onClick prop. 
-  The name of the prop itself is not that significant, but our naming choice 
-  wasn't completely random.*/
-  return (
-    <div>
-      <Display counter={counter}/>
-
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />  
-    </div>
-  )
 }
 
 
-/*let's place the application's state in the App component and pass 
-it down to the Display component through props:*/
-const Display = ({ counter }) => <div>{counter}</div>
+const App = () => {
+  //buttons  state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  //event handler
+  const clickHandlerGood = (newValue) => () => setGood(newValue)
+  const clickHandlerNeutral = (newValue) => () => setNeutral(newValue)
+  const clickHandlerBad = (newValue) => () => setBad(newValue)  
 
-/* let's make a Button component for the buttons of our application. 
-We have to pass the event handler as well as the title of the button 
-through the component's props:*/
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-/*When one of the buttons is clicked, the event handler is executed. 
-The event handler changes the state of the App component with the setCounter 
-function. Calling a function that changes the state causes the component to re-render.*/
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={clickHandlerGood(good+1)}>good</button>
+      <button onClick={clickHandlerNeutral(neutral+1)}>neutral</button>
+      <button onClick={clickHandlerBad(bad+1)}>bad</button>
+      
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
 
 export default App
