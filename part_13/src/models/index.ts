8c02a8da3357026fd7b80fefc58781d88806d63a
lucabk <lucabk96l@gmail.com>
2024-10-14 11:26:30 +0200

    ex 13.17

--- part_13/src/models/index.ts
+++ part_13/src/models/index.ts
@@ -6,13 +6,13 @@ User.hasMany(Blog)
 Blog.belongsTo(User)
 
 // Function to sync the Blog model
-async function syncallModels() {    
+/*async function syncallModels() {    
   await User.sync({ alter: true });
   await Blog.sync({ alter: true }); // Sequelize executes the command 'CREATE TABLE IF NOT EXISTS "blogs"...' to Postegres
 
   }
   
 // Call the sync function to db
-syncallModels().catch(console.error);
+syncallModels().catch(console.error);*/
 
 export { Blog, User }
--- part_13/src/utils/db.ts
+++ part_13/src/utils/db.ts
@@ -1,14 +1,33 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "./config";
+import { Umzug, SequelizeStorage } from "umzug";
 
 // Create a new instance of Sequelize using the database URL from environment variables
 export const sequelize = new Sequelize(DATABASE_URL as string, {
     dialect: 'postgres',
 });
 
+// Migration
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: '/home/luca/fullstackopen/Full-Stack-Open/part_13/src/migrations/*.ts',
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  })
+  
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
+
 export const connectToDatabase = async () => {
     try {
       await sequelize.authenticate()
+      await runMigrations()
       console.log('connected to the database')
     } catch (err) {
       console.log('failed to connect to the database',err)
