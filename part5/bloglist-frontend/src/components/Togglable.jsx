2091b27fca0de7ec60724328716ae68c8e8d19c7
lucabk <lucabk96l@gmail.com>
2024-11-20 21:13:38 +0100

    ex 5.9

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -17,7 +17,7 @@ const Togglable = (props) => {
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>hide</button>
+        <br></br><button onClick={toggleVisibility}>hide</button>
       </div>
     </div>
   )
