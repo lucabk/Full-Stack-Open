9b7d499d6ee7f5b2ec1d22bf4e1b289de0f27c2a
lucabk <lucabk96l@gmail.com>
2025-01-19 18:54:31 +0100

    ex 5.18

--- part5/playwright/tests/bloglist_app.spec.js
+++ part5/playwright/tests/bloglist_app.spec.js
@@ -3,9 +3,27 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 describe('Blog app', () => {
   beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
-  })
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(page.getByText('Login to application')).toBeVisible() //check form 
   })
+  
+  describe('Login', () => {
+    test('form is shown', async ({ page }) => {
+        await expect(page.getByText('Login to application')).toBeVisible() //check form
+    })
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.getByRole('textbox').first().fill("user2@example.com") //fill form
+        await page.getByRole('textbox').last().fill("password2")
+        await page.getByRole('button', { name: 'Login' }).click() //click Login button
+        await expect(page.getByText('User Two logged in')).toBeVisible()  
+    })
+    test('fails with incorrect credentials', async ({ page }) => {
+        await page.getByRole('textbox').first().fill("user2@example.com") //fill form
+        await page.getByRole('textbox').last().fill("wrongpassword")
+        await page.getByRole('button', { name: 'Login' }).click() //click Login button
+        const errorDiv = await page.locator('.error')
+        await expect(errorDiv).toContainText('Wrong credentials')
+        await expect(page.getByText('User Two logged in')).not.toBeVisible() 
+    })
+  })
+  
 })
