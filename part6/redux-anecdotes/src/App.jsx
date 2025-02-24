4090ed83f53f2fb4e7e9fe0943d9eb852c4c1522
lucabk <lucabk96l@gmail.com>
2025-02-24 14:24:45 +0100

    ex 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,22 @@ import AddAnectode from "./components/AnecdoteForm"
 import AnecdoteList from "./components/AnecdoteList"
 import Filter from "./components/Filter"
 import Notification from "./components/Notification"
+import * as service from "./services/anecdotes"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { setAnectodes } from "./reducers/anecdoteReducer"
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    service.getAll().then(res => {
+      console.log("anectodes GET from json server and store updated")
+      dispatch(setAnectodes(res))
+    })
+
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
